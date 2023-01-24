e9302c85adee18c2f5ef576e91d332ff584eb75a
draconical <draconiclove@gmail.com>
2023-01-24 21:05:12 +0300

    Установил Materials + разработка окна логина

--- src/app/components/auth/auth.module.ts
+++ src/app/components/auth/auth.module.ts
@@ -1,11 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { AuthComponent } from './auth.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AuthComponent, LoginComponent, RegistrationComponent],
   exports: [AuthComponent],
-  imports: [],
+  imports: [MatInputModule, MatButtonModule, ReactiveFormsModule, CommonModule],
 })
-export class ModuleModule {}
+export class AuthModule {}
--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +8,14 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent {
+  constructor() {}
 
+  authForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(4), Validators.pattern('^[A-Za-z][A-Za-z0-9]*$')]),
+    password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.pattern('^[A-Za-z][A-Za-z0-9]*$')])
+  });
+
+  checkControlValidation(formControlName: string, errorName: string): boolean {
+    return this.authForm.get(formControlName)?.hasError(errorName) || false;
+  }
 }
